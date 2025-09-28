90ea8297af081b3c5020a6a7e339007e6bab9d8c
Rega Satria Nugraha <59075378+regasatrian11@users.noreply.github.com>
2025-09-28 19:43:58 +0700

    Add files via upload

---
+++ hotspot/js/paketdata.js
@@ -0,0 +1,24 @@
+var paketData = [
+    {
+        nama: "PAKET 1 HARI",
+        harga: "3.000,-",
+        durasi: "1 HARI",
+        kuota: "Unlimited",
+        device: "Single Device"
+    },
+    {
+        nama: "PAKET 7 HARI",
+        harga: "18.000,-",
+        durasi: "7 HARI",
+        kuota: "Unlimited",
+        device: "Single Device"
+    },
+    {
+        nama: "PAKET 30 HARI",
+        harga: "55.000,-",
+        durasi: "30 HARI",
+        kuota: "Unlimited",
+        device: "Single Device"
+    }
+    // Cukup tambahkan paket baru di sini tanpa memikirkan ID atau warna.
+];
---
+++ hotspot/js/variables.js
@@ -0,0 +1,2 @@
+const hotspotName = "NGAMODAL.NET";
+const adminPhoneNumber = "0852-2222-8313"; // Ganti dengan nomor telepon yang benar
